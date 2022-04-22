e96be3d0d62d8a5967edae78e4412b58d1aefd48
palmaoff <fedorin.anton@mail.ru>
2022-04-22 14:08:44 +0300

    calculate function

--- Calculator.java
+++ Calculator.java
@@ -4,10 +4,38 @@ public class Calculator {
 
 	public static boolean IsValid(String str) {
 		boolean b = true;
+		Stack<Character> stack = new Stack<>();
 
 		// check all symbols
+		for (int i = 0; i < str.length(); i++) {
+			char c = str.charAt(i);
+			if (c >= '0' && c <= '9') continue;
+			else if (c == '+' || c == '-' || c == '*' || c == '/' || c == '(' || c == ')')
+				continue;
+			else {
+				b = false;
+				break;
+			}
+		}
 
 		// check bracets
+		for (int i = 0; i < str.length(); i++) {
+			char c = str.charAt(i);
+			if (c == '(') {
+				stack.push('(');
+			} else if (c == ')') {
+				if (stack.isEmpty()) {
+					b = false;
+					break;
+				} else {
+					stack.pop();
+				}
+			}
+		}
+
+		if (!stack.isEmpty()) {
+			b = false;
+		}
 
 		return b;
 	}
@@ -22,6 +50,11 @@ public class Calculator {
 		String result = "";
 		Stack<Character> stack = new Stack<>();
 		int priority;
+
+		str = RemoveWhiteSpaces(str);
+
+		if (!IsValid(str))
+			return "";
 
 		for (int i = 0; i < str.length(); i++) {
 			priority = GetPriority(str.charAt(i));
@@ -64,7 +97,32 @@ public class Calculator {
 	}
 
 	public double Calculate(String str) {
-		return 0;
+		String operand = new String();
+		Stack<Double> stack = new Stack<>();
+
+		for (int i = 0; i < str.length(); i++) {
+
+			if (GetPriority(str.charAt(i)) == 0 && str.charAt(i) != ' ') {
+				while (str.charAt(i) != ' ' && GetPriority(str.charAt(i)) == 0
+						&& i < str.length())
+					operand += str.charAt(i++);
+
+				stack.push(Double.parseDouble(operand));
+				operand = "";
+			}
+			if (GetPriority(str.charAt(i)) > 1) {
+				double a = stack.pop();
+				double b = stack.pop();
+
+				if (str.charAt(i) == '+') stack.push(b + a);
+				if (str.charAt(i) == '-') stack.push(b - a);
+				if (str.charAt(i) == '*') stack.push(b * a);
+				if (str.charAt(i) == '/') stack.push(b / a);
+
+			}
+		}
+		
+		return stack.pop();
 	}
 
 	private static int GetPriority(char c) {
--- Main.java
+++ Main.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 
 		Calculator c = new Calculator();
-		System.out.println(c.ToRPN("12+2-3"));
+		Scanner in = new Scanner(System.in);
+
+		System.out.print("Input an expression: ");
+        String str = in.nextLine();
+
+		in.close();
+
+		String rpn = c.ToRPN(str);
+		if (rpn != "") {
+			System.out.println(c.Calculate(rpn));
+		} else {
+			System.out.println("Expression not valid");
+		}
 
 	}
 
